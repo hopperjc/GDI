6439fd84dfaa9e57a931de91ab18b2512462ed8b
hopperjc <mhjc@cin.ufpe.br>
2021-11-16 23:30:20 -0300

    criação de tabelas

---
+++ criacaoTabelas.sql
@@ -0,0 +1,107 @@
+DROP TABLE pessoa
+DROP TABLE cargo
+DROP TABLE funcionario
+DROP TABLE cliente
+DROP TABLE estoque
+DROP TABLE produto
+DROP TABLE promoção
+DROP TABLE cartão_fidelidade
+DROP TABLE pedido
+DROP TABLE realizacao
+DROP TABLE contem
+DROP TABLE armazena
+
+CREATE TABLE Pessoa(
+    cpf VARCHAR2(14) NOT NULL,
+    nome_completo(40) NOT NULL,
+    telefone(12),
+    data_nascimento DATA NOT NULL,
+    cep NOT NULL, 
+    lagradouro NOT NULL, 
+    numero NOT NULL, 
+    complemento NOT NULL,
+    bairro NOT NULL,
+    CONSTRAINT pessoa_pk PRIMARY KEY (cpf)
+)
+
+CREATE TABLE cargo(
+    nome_cargo VARCHAR2(20) NOT NULL,
+    salario NUMBER NOT NULL
+)
+
+CREATE TABLE funcionario(
+    cpf_funcionario(14) NOT NULL,
+    supervisor VARCHAR2(14),
+    cargo VARCHAR2(20) NOT NULL,
+    CONSTRAINT funcionario_pk PRIMARY KEY (cpf_funcionario),
+    CONSTRAINT funcionario_cargo_fk FOREIGN KEY (cargo) REFERENCES cargo(nome_cargo),
+    CONSTRAINT funcionario_supervisor_fk FOREIGN KEY (supervisor) REFERENCES funcionario(cpf_funcionario)
+)
+
+CREATE TABLE cliente(
+    cpf_cliente(14) NOT NULL,
+    data_cadastro DATE NOT NULL,
+    CONSTRAINT cliente_pk PRIMARY KEY (cpf_cliente)
+)
+
+CREATE TABLE estoque(
+    id_estoque NUMBER NOT NULL,
+    qtd_produtos NUMBER,
+    CONSTRAINT estoque_pk PRIMARY KEY (id_estoque)
+)
+
+CREATE TABLE produto(
+    id_produto NUMBER NOT NULL, 
+    nome VARCHAR2(20) NOT NULL,
+    categoria VARCHAR2(20) NOT NULL,
+    preco NUMBER NOT NULL,
+    id_estoque NUMBER NOT NULL,
+    CONSTRAINT produto_pk PRIMARY KEY (id_produto),
+    CONSTRAINT produto_estoque_fk FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque)
+)
+
+CREATE TABLE promocao(
+    codigo_promocional NUMBER NOT NULL,
+    valor_desconto NUMBER NOT NULL,
+    data_termino DATE NOT NULL,
+    CONSTRAINT promocao_pk PRIMARY KEY (codigo_promocional)
+)
+
+CREATE TABLE cartao_fidelidade(
+    data_emicao DATE NOT NULL,
+    cpf_cliente VARCHAR2(14) NOT NULL, 
+    CONSTRAINT cartao_fidelidade_pk PRIMARY KEY (data_emicao, id_cliente)
+)
+
+CREATE TABLE pedido(
+    id_pedido NUMBER NOT NULL, 
+    data_pedido DATE NOT NULL,
+    cpf_funcionario_pedido VARCHAR2(14) NOT NULL,
+    cpf_cliente_pedido VARCHAR2(14) NOT NULL
+)
+
+CREATE TABLE realizacao(
+    id_pedido_realizacao NUMBER NOT NULL, 
+    codigo_promocional_realizacao NUMBER NOT NULL,
+    CONSTRAINT id_pedido_fk FOREIGN KEY (id_pedido_realizacao) REFERENCES pedido(id_pedido),
+    CONSTRAINT cdg_pomocional_fk FOREIGN KEY (codigo_promocional_realizacao) REFERENCES promocao(codigo_promocional)
+)
+
+CREATE TABLE contem(
+    id_pedido_contem NUMBER NOT NULL,
+    id_produto_contem NUMBER NOT NULL,
+    quantidade NUMBER,
+    CONSTRAINT contem_pedido_fk FOREIGN KEY (id_pedido_contem) REFERENCES pedido(id_pedido),
+    CONSTRAINT contem_produto_fk FOREIGN KEY (id_produto_contem) REFERENCES produto(id_produto)
+)
+
+CREATE TABLE armazena(
+    data_armazenagem DATE NOT NULL,
+    id_produto_armazena NUMBER NOT NULL,
+    id_estoque_armazena NUMBER NOT NULL,
+    cpf_funcionario_armazena VARCHAR2(14) NOT NULL,
+    CONSTRAINT armazena PRIMARY KEY (data_armazenagem),
+    CONSTRAINT armazena_produto_fk FOREIGN KEY (id_produto_armazena) REFERENCES produto(id_produto),
+    CONSTRAINT armazena_estoque_fk FOREIGN KEY (id_estoque_armazena) REFERENCES estoque(id_estoque),
+    CONSTRAINT armazena_funcionario_fk FOREIGN KEY (cpf_funcionario_armazena) REFERENCES funcionario(cpf_funcionario)
+)
