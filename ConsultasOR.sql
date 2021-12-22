1813b8c7b73de9b9ab8980785978917728556442
hopperjc <mhjc@cin.ufpe.br>
2021-12-22 00:38:32 -0300

    consultas

--- ConsultasOR.sql
+++ ConsultasOR.sql
@@ -14,13 +14,11 @@ END;
 -- Exiba as datas de emissão dos cartões fidelidade da Cliente de cpf '444.555.666-77'.
 SELECT * FROM TABLE (SELECT c.lista_cartao_fidelidade FROM tb_lista_cartao_fidelidade c WHERE c.cpf_cliente_cf = '444.555.666-77');
 /
-
 --Mude a data de emição do cartão fidelidade emitido em 08/10/0202 para 12/10/2020.
 UPDATE TABLE (SELECT c.lista_cartao_fidelidade FROM tb_lista_cartao_fidelidade c WHERE c.cpf_cliente_cf = '444.555.666-77') cf
     SET cf.data_emissao = to_date('12/10/2020', 'dd/mm/yy')
     WHERE cf.data_emissao = to_date('08/10/2020', 'dd/mm/yy')
-
-
+/
 -- Retorna o funcionario seu cargo e o supervisor.
 select DEREF(f.supervisor).nome_completo as supervisor, f.nome_completo as funcionario, DEREF(f.cargo).nome_cargo as cargo from tb_funcionario f;
 /
