8e720e0e88febeec2ee728a4e0c20d33b86e2908
hopperjc <mhjc@cin.ufpe.br>
2021-12-22 00:18:19 -0300

    consultas inicio

--- ConsultasOR.sql
+++ ConsultasOR.sql
@@ -3,21 +3,15 @@ SELECT DEREF(r.cpf_cliente_realizacao).nome_completo AS Nome,
         DEREF(r.id_pedido_realizacao).id_pedido as Pedido FROM tb_realizacao r
 WHERE DEREF(r.id_pedido_realizacao).id_pedido = 2;
 /
--- 
-SELECT DEREF(e.id_estoque) FROM tb_produto p 
-WHERE p.nome = 'coxinha';
+--Mostre o segundo telefone do gerente da loja 
+DECLARE
+n tb_funcionario.telefones%TYPE
+BEGIN 
+SELECT f.telefones INTO n FROM tb_funcionario f WHERE DEREF(f.cargo).nome_cargo = 'Gerente';
+DBMS_OUTPUT.PUT_LINE('Segundo telefone do gerente: ' || n(2).ddd || ' - ' || n(2).numero);
+END;
 /
-
-SELECT * FROM TABLE (SELECT c.lista_cartao_fidelidade FROM tb_lista_cartao_fidelidade_cliente c WHERE c.data_emissao = '08/10/2020');
-/
-
---Mude a data de emição do cartão fidelidade emitido em 08/10/0202 para 12/10/2020
-UDPATE TABLE (SELECT lista_cartoes FROM TABLE tb_lista_cartao_fidelidade_cliente
-                WHERE data_emissao = '08/10/2020') C SET C.data_emissao = '12/10/2020' 
-                WHERE (SELECT DEREF(l.cpf_cliente_cf) FROM tb_lista_cartao_fidelidade_cliente l, tb_cliente cl 
-                        WHERE l.cpf_client_cf = cl.cpf_cliente);
-/
-
+-- Exiba os detalhes da Cliente de nome completo 'Maria Augusta'.
 
 -- Retorna o funcionario seu cargo e o supervisor
 select DEREF(f.supervisor).nome_completo as supervisor, f.nome_completo as funcionario, DEREF(f.cargo).nome_cargo as cargo from tb_funcionario f;
