21638da7c01ab0de8dd2782dae2e44f4e1fab365
hopperjc <mhjc@cin.ufpe.br>
2021-12-23 09:48:53 -0300

    funcionario OK Encomendas OK

--- MongoDB/Documents/encomendas.js
+++ MongoDB/Documents/encomendas.js
@@ -1,10 +1,74 @@
 db.encomenda.insertMany([
     {
-        _id: 1, 
-        nome: "produto", 
+        _id: 1,
         peso: 10, 
         comprimento: 12, 
         largura: 9,
-        cliente: 1
-    }
+        altura: 4,
+        cliente: 1,
+        custo: 70.00
+    },
+    {
+        _id: 2,
+        peso: 6, 
+        comprimento: 3, 
+        largura: 5,
+        altura: 4,
+        cliente: 2,
+        custo: 37.00
+    },
+    {
+        _id: 3,
+        peso: 100, 
+        comprimento: 120, 
+        largura: 90,
+        altura: 50,
+        cliente: 3,
+        custo: 700.00
+    },
+    {
+        _id: 4,
+        peso: 25, 
+        comprimento: 29, 
+        largura: 42,
+        altura: 17,
+        cliente: 4,
+        custo: 175.00
+    },
+    {
+        _id: 5,
+        peso: 17.5, 
+        comprimento: 18, 
+        largura: 18,
+        altura: 18,
+        cliente: 5,
+        custo: 122.50
+    },
+    {
+        _id: 6,
+        peso: 40, 
+        comprimento: 50, 
+        largura: 30,
+        altura: 10,
+        cliente: 6,
+        custo: 280.00
+    },
+    {
+        _id: 7,
+        peso: 250, 
+        comprimento: 200, 
+        largura: 150,
+        altura: 50,
+        cliente: 3,
+        custo: 1750.00
+    },
+    {
+        _id: 8,
+        peso: 2, 
+        comprimento: 3, 
+        largura: 3,
+        altura: 4,
+        cliente: 5,
+        custo: 14.00
+    },
 ])
--- MongoDB/Documents/entregas.js
+++ MongoDB/Documents/entregas.js
@@ -2,7 +2,7 @@
 db.entregas.insertMany([
   {
       _id: 1, 
-      destinatario: 1,
+      encomenda: 1,
       entregador: 1,
       destino: 2,
       horario_saida: new Date("2021-10-12"),
