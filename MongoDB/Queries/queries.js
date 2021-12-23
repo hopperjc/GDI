8ea1cba9aefcd6be2d7b156e55780835b80a65fb
hopperjc <mhjc@cin.ufpe.br>
2021-12-23 10:33:32 -0300

    funcionario OK Encomendas OK cliente OK endereços OK entregas OK

--- MongoDB/Queries/queries.js
+++ MongoDB/Queries/queries.js
@@ -6,9 +6,9 @@ db.encomenda.find({ client: { $exists: true, $all: [2] } }).pretty();
 // Encontre os clientes que possuem 2 endereços
 db.cliente.find({ endereco: { $size: 2 } }).pretty();
 
-// Listar email, telefone e sexo e endereço principa do cliente de nome "Carlos Manoel" 
+// Listar email, telefone, sexo e endereço principa do cliente de nome "Carlos Manoel" 
 db.cliente.aggregate([
-    { $match: { Nome: /^Carlos Manoel/i } },
+    { $match: { nome: /^Carlos Manoel/i } },
     { $sort: { endere_principal: 1 } },
-    { $project: { _id: 0, Nome: 0, email: 1, telefone: 1, sexo: 1, endere_principal: 1, enderecos: 0 } },
+    { $project: { _id: 0, nome: 0, email: 1, telefone: 1, sexo: 1, endere_principal: 1, enderecos: 0 } },
   ]).pretty();
