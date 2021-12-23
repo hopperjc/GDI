f2e6dfd3983970ff0198c567aea2d1bd8d89a442
hopperjc <mhjc@cin.ufpe.br>
2021-12-23 10:49:50 -0300

    funcionario OK Encomendas OK cliente OK endereços OK entregas OK

--- MongoDB/Queries/queries.js
+++ MongoDB/Queries/queries.js
@@ -1,4 +1,4 @@
-//use mydb;
+use db;
 
 // Encontre a encomenda que pertence ao CLiente de id 2
 db.encomenda.find({ client: { $exists: true, $all: [2] } }).pretty();
@@ -12,3 +12,8 @@ db.cliente.aggregate([
     { $sort: { endere_principal: 1 } },
     { $project: { _id: 0, nome: 0, email: 1, telefone: 1, sexo: 1, endere_principal: 1, enderecos: 0 } },
   ]).pretty();
+
+// search Rua 2
+db.enderecos.findOne({
+    $text: { $search: "Rua 2" }
+})
