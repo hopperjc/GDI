a4bde05a358ce04f51c8972601ecc8d0a3521733
hopperjc <mhjc@cin.ufpe.br>
2021-12-23 09:31:16 -0300

    funcionario OK cliente falta endereços

--- MongoDB/Documents/clientes.js
+++ MongoDB/Documents/clientes.js
@@ -2,10 +2,66 @@
 db.cliente.insertMany([
     {
         _id: 1,
-        nome : "José Augusto",
-        cpf : "111.222.333-44",
-        email : "jose@entregas.com.br",
-        telefone : 81999999999,
+        nome : "Carlos Antonio",
+        cpf : "999.888.777-66",
+        email : "carlinhos@gmail.com.br",
+        telefone : 81999998888,
+        sexo : "M",
+        enderecos: [
+            {
+                endereco: 1,
+                principal: true
+            }
+        ]
+    },
+    {
+        _id: 2,
+        nome : "Maria Clara",
+        cpf : "888.777.666-55",
+        email : "mclara@gmail.com.br",
+        telefone : 81888887777,
+        sexo : "F",
+        enderecos: [
+            {
+                endereco: 1,
+                principal: true
+            }
+        ]
+    },
+    {
+        _id: 3,
+        nome : "Carlos Manoel",
+        cpf : "777.666.555-44",
+        email : "cmgato@gmail.com.br",
+        telefone : 81777776666,
+        sexo : "M",
+        enderecos: [
+            {
+                endereco: 1,
+                principal: true
+            }
+        ]
+    },
+    {
+        _id: 4,
+        nome : "Leticia Torres",
+        cpf : "666.555.444-33",
+        email : "let@gmail.com.br",
+        telefone : 81666665555,
+        sexo : "F",
+        enderecos: [
+            {
+                endereco: 1,
+                principal: true
+            }
+        ]
+    },
+    {
+        _id: 5,
+        nome : "Fernando Chaves",
+        cpf : "555.444.333-22",
+        email : "nandinho@gmail.com.br",
+        telefone : 81555554444,
         sexo : "M",
         enderecos: [
             {
--- MongoDB/Documents/funcionarios.js
+++ MongoDB/Documents/funcionarios.js
@@ -1,5 +1,5 @@
 // Criando coleção de funcionários
-db.funcionarios.insertMany([
+db.funcionarios.insert([
     {
         _id: 1,
         nome : "José Augusto",
@@ -7,7 +7,8 @@ db.funcionarios.insertMany([
         dataNascimento : "14/08/1979",
         email : "jose@entregas.com.br",
         telefone : 81999999999,
-        sexo : "M"
+        sexo : "M",
+        NIT : "999.99999.99-9"
     },
     {
         _id: 2,
@@ -16,7 +17,8 @@ db.funcionarios.insertMany([
         dataNascimento: "11/07/1987",
         email: "anac@entregas.com.br",
         telefone: 81888888888,
-        sexo: "F"
+        sexo: "F",
+        NIT : "888.88888.88-8"
     },
     {
         _id: 3,
@@ -25,7 +27,8 @@ db.funcionarios.insertMany([
         dataNascimento: "04/01/1989",
         email: "helena@entregas.com.br",
         telefone: 81777777777,
-        sexo: "F"
+        sexo: "F",
+        NIT : "777.77777.77-7"
     },
     {
         _id: 4,
@@ -34,7 +37,8 @@ db.funcionarios.insertMany([
         dataNascimento : "03/04/1994",
         email : "lucas@entregas.com.br",
         telefone : 81666666666,
-        sexo : "M"
+        sexo : "M",
+        NIT : "666.66666.66-6"
     },
     {
         _id: 5,
@@ -43,8 +47,7 @@ db.funcionarios.insertMany([
         dataNascimento : "23/04/1996",
         email : "isa@entregas.com.br",
         telefone : 81555555555,
-        sexo : "F"
+        sexo : "F",
+        NIT : "555.55555.55-5"
     }
 ])
-// =-=-=-= DELETE =-=-=-=
-db.funcionarios.deleteMany({salario: {$gt: 3000}})
